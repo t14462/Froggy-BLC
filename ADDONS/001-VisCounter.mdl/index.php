a373328770893d70f773e7fc45d037fe323f2682
t14462 <timofeevsv1989@gmail.com>
2025-11-23 04:24:53 +0100

    iframe minus background

--- ADDONS/001-VisCounter.mdl/index.php
+++ ADDONS/001-VisCounter.mdl/index.php
@@ -16,4 +16,4 @@ require_once __DIR__ . "/../GetRelStandalone.php";
 
 
 
-$menubar .= "<iframe src='".relPath(__DIR__)."visitors.php' title='Счётчик посетителей Онлайн.' style='background: transparent;'></iframe>";
+$menubar .= "<iframe src='".relPath(__DIR__)."visitors.php' title='Счётчик посетителей Онлайн.'></iframe>";
