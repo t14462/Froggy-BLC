7ef4fdf789a0dec1de69fae5042ec78dc61d150b
t14462 <timofeevsv1989@gmail.com>
2026-03-26 03:42:16 +0100

    obyava + <br />

--- SYSTEM/modules/functions.php
+++ SYSTEM/modules/functions.php
@@ -3259,7 +3259,7 @@ function obyava() {
         $obstring = typograph_guillemets($obstring);
         $obstring = ru_nbsp_typograf($obstring);
 
-        $obstring = "<aside id='obyava'>$obstring</aside>";
+        $obstring = "<br /><aside id='obyava'>$obstring</aside>";
 
     } else {
 
