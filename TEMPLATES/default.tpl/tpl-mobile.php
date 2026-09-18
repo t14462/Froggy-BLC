90dbfc3c418bb9a548818b5cbe4eea2b3550a9fe
t14462 <timofeevsv1989@gmail.com>
2026-09-19 00:57:42 +0200

    Mobile MScroll-links inner space

--- TEMPLATES/default.tpl/tpl-mobile.php
+++ TEMPLATES/default.tpl/tpl-mobile.php
@@ -55,6 +55,6 @@ if (!defined('SECURE_ACCESS')) { die('Direct access not permitted'); }
 <!-- <p class="mscroll">&nbsp;<a href="#system-links">ВВЕРХ!</a>&nbsp;&nbsp;<a href="#comm-title">Коммент</a>&nbsp;&nbsp;<a href="#sitemenu">Меню</a></p> -->
 </aside>
 <footer><?php echo $footer; ?></footer>
-<div class="mscroll"><a href="#system-links">ВВЕРХ!</a>&nbsp;<a href="#comm-title">Коммент</a>&nbsp;<a href="#to-menu-bar">Меню</a></div>
+<div class="mscroll"><a href="#system-links">ВВЕРХ!</a><a href="#comm-title">Коммент</a><a href="#to-menu-bar">Меню</a></div>
 </body>
 </html>
